d64e26ea891ddb6917d45e868461715d1e8412ee
Will <wdnconsultancy@yahoo.com>
2025-10-13 09:27:52 +0800

    provide a mvp for question service, able to get question based on topic and difficulty

--- services/question/src/index.ts
+++ services/question/src/index.ts
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
-import express, { Request, Response } from "express";
+import express, {Request, Response} from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import {Question} from "./models/question";
+import {sampleQuestions} from "./seed/sampleQuestions";
 
 dotenv.config();
 
@@ -22,9 +24,27 @@ async function connectToMongoDB() {
     try {
         await mongoose.connect(process.env.MONGO_URL as string);
         console.log("Successfully connected to MongoDB");
+        const dbCount = await Question.countDocuments();
+        // seed mongoDB if empty 
+        if (dbCount === 0) {
+            await seedEmptyDb();
+        } else {
+            console.log(`DB has ${dbCount} questions`)
+        }
     } catch (error) {
         console.error("Failed to connect to MongoDB, user service shutting down");
         process.exit(1);
+    }
+}
+
+// to do proper seeding in the future from codeforces
+async function seedEmptyDb() {
+    try {
+        console.log("DB is empty, seeding with sample questions...");
+        await Question.insertMany(sampleQuestions);
+        console.log(`DB was successfully seeded, the DB now has has ${sampleQuestions.length} questions`);
+    } catch (error) {
+        console.error("Failed to seed DB: ", error);
     }
 }
 
@@ -35,11 +55,43 @@ questionApp.listen(PORT, "0.0.0.0", () => {
     console.log(`Question service running on http://localhost:${PORT}`);
   });
 
-// create
+// create (admin)
 
-// read
+// read (user + admin)
+// read random qn (no topic or difficulty given)
 
-// update
+// read random qn based on topic
 
-// delete 
+// read random qn based on difficulty
+
+// read random qn based on topic and difficulty
+questionApp.get("/api/question_service/random/topic/:topic/difficulty/:difficulty",
+  async(req:Request, res:Response) => {
+    try {
+        const {topic, difficulty} = req.params;
+        // aggregate returns an array, so must index 0 even if its just one value
+        // can use aggregate as mongoDB inbuilt randomizer 
+        //https://stackoverflow.com/questions/2824157/how-can-i-get-a-random-record-from-mongodb
+        const matchingQuestions = await Question.aggregate([
+            {
+                $match: {
+                    topic,
+                    difficulty
+                }
+            },
+            {
+                $sample: {size: 1}    
+            }
+        ]);
+        return res.json(matchingQuestions[0]);
+    } catch(error) {
+        console.error("Failed to read a random question based on difficulty and topic: ", error);
+        return res.status(500).json({ error: "Failed"})
+    }
+  });
+// read all topics
+
+// update (admin)
+
+// delete (admin)
 
---
+++ services/question/src/seed/sampleQuestions.ts
@@ -0,0 +1,165 @@
+export const sampleQuestions = [
+    {
+        title: "DP1",
+        description: "TBD",
+        difficulty: "Easy",
+        topic: "DP"
+    },
+    {
+        title: "DP2",
+        description: "TBD",
+        difficulty: "Easy",
+        topic: "DP"
+    },
+    {
+        title: "DP3",
+        description: "TBD",
+        difficulty: "Easy",
+        topic: "DP"
+    },
+    {
+        title: "DP4",
+        description: "TBD",
+        difficulty: "Medium",
+        topic: "DP"
+    },
+    {
+        title: "DP5",
+        description: "TBD",
+        difficulty: "Medium",
+        topic: "DP"
+    },
+    {
+        title: "DP6",
+        description: "TBD",
+        difficulty: "Medium",
+        topic: "DP"
+    },
+    {
+        title: "DP7",
+        description: "TBD",
+        difficulty: "Hard",
+        topic: "DP"
+    },
+    {
+        title: "DP8",
+        description: "TBD",
+        difficulty: "Hard",
+        topic: "DP"
+    },
+    {
+        title: "DP9",
+        description: "TBD",
+        difficulty: "Hard",
+        topic: "DP"
+    },
+    {
+        title: "Search1",
+        description: "TBD",
+        difficulty: "Easy",
+        topic: "Searching"
+    },
+    {
+        title: "Search2",
+        description: "TBD",
+        difficulty: "Easy",
+        topic: "Searching"
+    },
+    {
+        title: "Search3",
+        description: "TBD",
+        difficulty: "Easy",
+        topic: "Searching"
+    },
+    {
+        title: "Search4",
+        description: "TBD",
+        difficulty: "Medium",
+        topic: "Searching"
+    },
+    {
+        title: "Search5",
+        description: "TBD",
+        difficulty: "Medium",
+        topic: "Searching"
+    },
+    {
+        title: "Search6",
+        description: "TBD",
+        difficulty: "Medium",
+        topic: "Searching"
+    },
+    {
+        title: "Search7",
+        description: "TBD",
+        difficulty: "Hard",
+        topic: "Searching"
+    },
+    {
+        title: "Search8",
+        description: "TBD",
+        difficulty: "Hard",
+        topic: "Searching"
+    },
+    {
+        title: "Search9",
+        description: "TBD",
+        difficulty: "Hard",
+        topic: "Searching"
+    },
+    {
+        title: "Tree1",
+        description: "TBD",
+        difficulty: "Easy",
+        topic: "Trees"
+    },
+    {
+        title: "Tree2",
+        description: "TBD",
+        difficulty: "Easy",
+        topic: "Trees"
+    },
+    {
+        title: "Tree3",
+        description: "TBD",
+        difficulty: "Easy",
+        topic: "Trees"
+    },
+    {
+        title: "Tree4",
+        description: "TBD",
+        difficulty: "Medium",
+        topic: "Trees"
+    },
+    {
+        title: "Tree5",
+        description: "TBD",
+        difficulty: "Medium",
+        topic: "Trees"
+    },
+    {
+        title: "Tree6",
+        description: "TBD",
+        difficulty: "Medium",
+        topic: "Trees"
+    },
+    {
+        title: "Tree7",
+        description: "TBD",
+        difficulty: "Hard",
+        topic: "Trees"
+    },
+    {
+        title: "Tree8",
+        description: "TBD",
+        difficulty: "Hard",
+        topic: "Trees"
+    },
+    {
+        title: "Tree9",
+        description: "TBD",
+        difficulty: "Hard",
+        topic: "Trees"
+    },
+
+]
