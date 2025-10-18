53e7d1afa06ad6bdedf895f8510c0609c3caa621
BlazeChron <jasonjhl20@gmail.com>
2025-10-18 14:04:56 +0800

    Patch vulnerable code

--- services/collab/server.js
+++ services/collab/server.js
@@ -5,10 +5,7 @@ const { Server } = require('socket.io');
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-      }
+
 });
 
 io.on('connection', (socket) => {
