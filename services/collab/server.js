eaf210c19ff83d8dde8350375d2345cee20fc41c
BlazeChron <jasonjhl20@gmail.com>
2025-10-18 16:10:14 +0800

    Add backend docker

--- services/collab/server.js
+++ services/collab/server.js
@@ -5,7 +5,10 @@ const { Server } = require('socket.io');
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-
+  cors: {
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    credentials: true,
+  },
 });
 
 io.on('connection', (socket) => {
