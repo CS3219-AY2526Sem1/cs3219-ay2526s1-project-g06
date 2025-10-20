0d2cdfbad0ae5c087440b086e514c6f4cbd0adea
BlazeChron <jasonjhl20@gmail.com>
2025-10-20 11:49:55 +0800

    Add working rooms

--- frontend/peerprep-web/src/pages/Collab.tsx
+++ frontend/peerprep-web/src/pages/Collab.tsx
@@ -2,10 +2,12 @@ import React, {useEffect, useRef, useState} from "react";
 import {io, Socket} from "socket.io-client";
 
 //const SOCKET_SERVER_URL = import.meta.env.VITE_BACKEND_URL ? `${import.meta.env.VITE_BACKEND_URL}:4004` : "http://localhost:4004"
-const SOCKET_SERVER_URL = "https://16.176.159.10:4004";
+//const SOCKET_SERVER_URL = "https://16.176.159.10:4004";
+const SOCKET_SERVER_URL = "http://localhost:4004";
 
 const CollabComponent = () => {
   const [codespaceContent, setCodespaceContent] = useState("");
+  const [currRoom, setCurrRoom] = useState("");
   const socketRef = useRef<Socket | null>(null);
 
   useEffect(() => {
@@ -26,13 +28,35 @@ const CollabComponent = () => {
     const newText = e.target.value;
     setCodespaceContent(newText);
     if (socketRef.current) {
-      socketRef.current.emit('codespace change', newText);
+      if (currRoom) {
+        socketRef.current.emit('codespace change', newText);
+      }
     }
+  }
+
+  const handleRoomChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    console.log(e.target.value);
+    if (!socketRef.current) {
+      return;
+    }
+		if (currRoom) {
+	    socketRef.current.emit("leave-room", currRoom);
+	  }
+	
+	  setCurrRoom(e.target.value);
+	  socketRef.current.emit("join-room", e.target.value);
   }
   
   return (
     <div className="collab-container">
       <h1>Collaborative Codespace</h1>
+      <h2>{currRoom}</h2>
+      <textarea
+        id="roomdId" 
+        onChange={handleRoomChange}
+        rows={1} 
+        cols={8}
+      />
       <textarea
         id="codespace" 
         value={codespaceContent}
