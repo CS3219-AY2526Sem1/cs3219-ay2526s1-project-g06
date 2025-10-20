18b3335fc5e09a8714dfdd0fbaab42c5782bade5
BlazeChron <jasonjhl20@gmail.com>
2025-10-20 11:49:29 +0800

    Add working rooms

--- services/collab/server.js
+++ services/collab/server.js
@@ -13,17 +13,44 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
+  let currRoom;
   console.log('a user connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
   socket.on('codespace change', (text) => {
-    console.log(text);
-    io.emit('codespace change', text);
+    console.log(`sending ${currRoom} ${text}`);
+    io.to(currRoom).emit('codespace change', text);
+  });
+
+  socket.on('join-room', (room) => {
+    socket.join(room);
+    currRoom = room;
+    console.log('user joined ' + room);
+    console.log("rooms in: ");
+    socket.rooms.forEach(room => {
+        if (room !== socket.id) {
+          console.log(room);
+            }
+    });
+  });
+  socket.on('leave-room', (room) => {
+    if (!currRoom) {
+      return;
+    }
+    console.log("rooms in: ");
+    socket.rooms.forEach(room => {
+        if (room !== socket.id) {
+          console.log(room);
+              socket.leave(room);
+            }
+    });
+    //socket.leave(room);
+    //console.log('user left ' + room);
   });
 });
 
 server.listen(4004, () => {
   console.log('server running at http://localhost:4004');
-  console.log(process.env.CORS_ORIGIN);
+  //console.log(process.env.CORS_ORIGIN);
 });
