4e8a931face72296f001bad3d15bab177b090c89
BlazeChron <jasonjhl20@gmail.com>
2025-11-05 13:51:23 +0800

    Fix bug

--- services/history/index.js
+++ services/history/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 
 const server = express();
 const PORT = process.env.PORT || 4005;
-const MONGO_URL = process.env.MONGO_URL ? process.env.MONGO_URL | 'mongodb://127.0.0.1:27017/test';
+const MONGO_URL = process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://127.0.0.1:27017/test';
 
 const getCorsOrigins = () => {
   if (process.env.NODE_ENV === 'production') {
