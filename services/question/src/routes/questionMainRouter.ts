4cc2d03265956d55484c3712174db9f64035de00
Will <wdnconsultancy@yahoo.com>
2025-11-10 10:01:13 +0800

    add user and admin authentication to question service

--- services/question/src/routes/questionMainRouter.ts
+++ services/question/src/routes/questionMainRouter.ts
@@ -3,17 +3,23 @@ import {createRouter} from "./subRouters/createRouter";
 import {readRouter} from "./subRouters/readRouter";
 import {updateRouter} from "./subRouters/updateRouter";
 import {deleteRouter} from "./subRouters/deleteRouter";
+import {checkUser} from "../middleware/userAuth";
+import {checkAdmin} from "../middleware/adminAuth";
 
 const router = Router();
 
-// TODO (compulsory): ensuring only admins can access certain routes  
-// TODO (optional-compulsory if performance lacking): cache + cache invalidation (reset) on create, update, delete operations. Issue is edge case of staleness. 
-// TODO (optional): create multiple qns with checks (slow), multiple calls to single qn
-// TODO (optional): create multiple questions (fast), no checks (use carefully)
-// TODO (optional): update multiple qns 
-// TODO (optional): delete multiple qns based on ids
+//new branch
+// TODO: ensuring only users authenticated admins can access certain routes  
+// TODO: Easy, Medium, Hard ordering 
+
+// user validation
+router.use(checkUser);
 
 router.use(readRouter);
+
+// admin validation
+router.use(checkAdmin);
+
 router.use(createRouter);
 router.use(updateRouter);
 router.use(deleteRouter);
